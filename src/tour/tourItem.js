067ab34dd2591eb45c5362a2d53c4a9770e782fd
hasujeong <75551721+jwebdew@users.noreply.github.com>
2023-10-11 15:07:46 +0900

    Update tourItem.js

--- src/tour/tourItem.js
+++ src/tour/tourItem.js
@@ -11,33 +11,38 @@ const TourItem = ({article}) => {
     } = article;
 
 
-    const myClick = () => {
     let my = document.querySelectorAll('.my');
     let line = document.querySelectorAll('.line');
     let fill = document.querySelectorAll('.fill');
 
-    for (let i = 0; i < my.length; i++) {
-        // my[i].onclick = function(){
-        //     if (line[i].style.display === 'block') {
-        //       line[i].style.display = 'none';
-        //       fill[i].style.display = 'block';
-        //     } else {
-        //       line[i].style.display = 'block';
-        //       fill[i].style.display = 'none';
-        //     };
-        //   };
-        my[i].onclick = () => {
-            
-            if (line[i].style.display === 'none') {
-              line[i].style.display = 'block';
-              fill[i].style.display = 'none';
-            } else {
-              line[i].style.display = 'none';
-              fill[i].style.display = 'block';
-            };
-          };
-    };
-}
+    line.forEach(item => {
+        item.style.display = 'block'
+    });
+    const myClick = () => {
+       
+        for (let i = 0; i < my.length; i++) {
+            my[i].onclick = function(){
+                if (line[i].style.display === 'block') {
+                  line[i].style.display = 'none';
+                  fill[i].style.display = 'block';
+                } else {
+                  line[i].style.display = 'block';
+                  fill[i].style.display = 'none';
+                };
+              };
+            // my[i].onclick = () => {
+                
+            //     if (line[i].style.display === 'none') {
+            //     line[i].style.display = 'block';
+            //     fill[i].style.display = 'none';
+            //     } else {
+            //     line[i].style.display = 'none';
+            //     fill[i].style.display = 'block';
+            //     };
+            // };
+        };
+    }
+    myClick();
     
     return (
         <div className="item">
