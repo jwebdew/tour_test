d9166cdad70ba239201b2433d3e65d120d156343
hasujeong <75551721+jwebdew@users.noreply.github.com>
2023-10-12 10:43:29 +0900

    Add files via upload

--- src/tour/tourItem.js
+++ src/tour/tourItem.js
@@ -16,12 +16,13 @@ const TourItem = ({article}) => {
         let my = document.querySelectorAll('.my');
         let line = document.querySelectorAll('.line');
         let fill = document.querySelectorAll('.fill');
+        let myChildren = my.childNodes;
         
         
         line.forEach(reset => {
             reset.style.display = 'block';
         });
-        for (let i = 0; i < my.length; i++) {
+        for (let i = 0; i < line.length; i++) {
             my[i].onclick = () => {
                 // if (line[i].style.display === 'block') {
                 //   line[i].style.display = 'none';
@@ -41,7 +42,7 @@ const TourItem = ({article}) => {
         };
     };
     
-    // myClick();
+    myClick();
 
     return (
         <div className="item">
